cfb8280b9577c019276d3d11f84ff5378674e9e8
micha <micha@LAPTOP-BFBQ551T>
2020-06-09 05:33:36 +0200

    I piu piu laserkami

--- ProjektNaLaby/src/Window_package/StartStopButton.java
+++ ProjektNaLaby/src/Window_package/StartStopButton.java
@@ -4,54 +4,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.swing.JButton;
 
+import Simulation.Laser;
 import Simulation.SimulationSettings;
 import Simulation.Spaceship;
+import Window_package.StartStopButton.LaserListener;
 
 public class StartStopButton extends JButton {
 
 	private static final long serialVersionUID = 1L;
+	
+	static ExecutorService exec;
 
 	public StartStopButton() 
 	{
 		super("START");
 		
-		addKeyListener(new KeyListener() {
-			
-			@Override
-			public void keyTyped(KeyEvent e) {	}	
-			
-			@Override
-			public void keyReleased(KeyEvent e) 
-			{
-				Spaceship.setArrowKeyAccX(0.0);
-				Spaceship.setArrowKeyAccY(0.0);								
-			}
-			
-			@Override
-			public void keyPressed(KeyEvent e) 
-			{
-				switch(e.getKeyCode())
-				{
-				case KeyEvent.VK_LEFT: 
-					Spaceship.setArrowKeyAccX(-Spaceship.thrust/SimulationSettings.getMass());
-					break;
-				case KeyEvent.VK_RIGHT: 
-					Spaceship.setArrowKeyAccX(Spaceship.thrust/SimulationSettings.getMass());
-					break;
-				case KeyEvent.VK_UP: 
-					Spaceship.setArrowKeyAccY(Spaceship.thrust/SimulationSettings.getMass());
-					break;
-				case KeyEvent.VK_DOWN:
-					Spaceship.setArrowKeyAccY(-Spaceship.thrust/SimulationSettings.getMass());
-					break;
-				default: 
-					break;
-				}
-			}
-		});
+		addKeyListener(new ControlListener());//sterowanie strza³kami
 		
 		addActionListener(new ActionListener() {// Rozpoczyna symulacjê ruchu statku
 			
@@ -74,9 +49,103 @@ public class StartStopButton extends JButton {
 				MainMenu.parameters += (new Double(SimulationSettings.getMass()).toString());
 				MainMenu.parameters += ('\n');
 				VectorPanel.startSpaceshipThread(); //statek zaczyna dzialac
+				MainClass.frame.getVectorPanel().addMouseListener(new LaserListener());
 			}
 		});
 
 	}
 	
+	class ControlListener implements KeyListener {
+
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			switch(arg0.getKeyCode())
+			{
+			case KeyEvent.VK_LEFT: 
+				Spaceship.setArrowKeyAccX(-Spaceship.thrust/SimulationSettings.getMass());
+				break;
+			case KeyEvent.VK_A:
+				Spaceship.setArrowKeyAccX(-Spaceship.thrust/SimulationSettings.getMass());
+				break;
+			case KeyEvent.VK_RIGHT: 
+				Spaceship.setArrowKeyAccX(Spaceship.thrust/SimulationSettings.getMass());
+				break;
+			case KeyEvent.VK_D: 
+				Spaceship.setArrowKeyAccX(Spaceship.thrust/SimulationSettings.getMass());
+				break;
+			case KeyEvent.VK_UP: 
+				Spaceship.setArrowKeyAccY(Spaceship.thrust/SimulationSettings.getMass());
+				break;
+			case KeyEvent.VK_W: 
+				Spaceship.setArrowKeyAccY(Spaceship.thrust/SimulationSettings.getMass());
+				break;
+			case KeyEvent.VK_DOWN:
+				Spaceship.setArrowKeyAccY(-Spaceship.thrust/SimulationSettings.getMass());
+				break;
+			case KeyEvent.VK_S:
+				Spaceship.setArrowKeyAccY(-Spaceship.thrust/SimulationSettings.getMass());
+				break;
+			default: 
+				break;
+			}
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			Spaceship.setArrowKeyAccX(0.0);
+			Spaceship.setArrowKeyAccY(0.0);	
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	class LaserListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if(!VectorPanel.getLaser().getKeepRunning())
+			{
+				int x = e.getX() - (MainClass.frame.getVectorPanel().getWidth()/2);
+				int y =  (MainClass.frame.getVectorPanel().getHeight()/2) - e.getY();
+				exec = Executors.newFixedThreadPool(1);
+				VectorPanel.setLaser(new Laser());
+				VectorPanel.setLaser(new Laser(VectorPanel.getSpaceship1(), x, y, MainClass.frame.getVectorPanel()));
+				exec.execute(VectorPanel.getLaser());
+				exec.shutdown();
+			}
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 }
